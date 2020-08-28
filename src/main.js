e0b5425587e18af03f9b5718bb7bcce18b192598
dvnikolaev <dvnikolaeev@gmail.com>
2020-08-28 08:53:07 +0400

    feat: add vue directive click-outside

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.directive('click-outside', {
+  bind(el, binding, vnode) {
+    el.clickOutside = function (event) {
+      if (el !== event.target || !el.contains(event.target)) {
+        vnode.context[binding.expression](event);
+      }
+    };
+    document.body.addEventListener('click', el.clickOutside)
+  },
+  unbind(el) {
+    document.body.removeEventListener('click', el.clickOutside)
+  },
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
