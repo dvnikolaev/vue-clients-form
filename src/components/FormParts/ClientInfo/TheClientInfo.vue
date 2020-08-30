d1fdc1a2da6f10fa2ba3a5fd5ae4c5d47305558f
dvnikolaev <dvnikolaeev@gmail.com>
2020-08-30 08:04:51 +0400

    feat: add props for components

--- src/components/FormParts/ClientInfo/TheClientInfo.vue
+++ src/components/FormParts/ClientInfo/TheClientInfo.vue
@@ -1,24 +1,47 @@
 <template>
   <div>
-    <ClientInitials v-if="activePage === 1"/>
-    <ClientInfo v-if="activePage === 2"/>
+    <ClientInitials
+      v-if="activePage === 1"
+      :firstName="firstName"
+      :lastName="lastName"
+      :middleName="middleName"
+      @change:formValue="changeFormValue"
+    />
+    <ClientInfo
+      v-if="activePage === 2"
+      :birthday="birthday"
+      :phone="phone"
+      :gender="gender"
+      @change:formValue="changeFormValue"
+    />
     <MedInfo v-if="activePage === 3" />
   </div>
 </template>
 
 <script>
-import ClientInitials from './ClientInitials';
-import ClientInfo from './ClientInfo';
-import MedInfo from './MedInfo';
+import ClientInitials from "./ClientInitials";
+import ClientInfo from "./ClientInfo";
+import MedInfo from "./MedInfo";
 
 export default {
   props: {
-    activePage: Number
+    activePage: Number,
+    firstName: String,
+    lastName: String,
+    middleName: String,
+    birthday: String,
+    phone: String,
+    gender: String
+  },
+  methods: {
+    changeFormValue({ fieldName, formValue }) {
+      this.$emit("change:formValue", { fieldName, formValue });
+    },
   },
   components: {
     ClientInitials,
     ClientInfo,
-    MedInfo
-  }
-}
+    MedInfo,
+  },
+};
 </script>
