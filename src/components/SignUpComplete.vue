28f8977466bb5225794057c773977de436ae9e17
dvnikolaev <dvnikolaeev@gmail.com>
2020-08-31 09:19:37 +0400

    feat: complete sign up component

---
+++ src/components/SignUpComplete.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="complete">
+    <div class="complete__circle"></div>
+    <h2 class="complete__header">Регистрация прошла успешно!</h2>
+  </div>
+</template>
+
+<style lang="scss">
+.complete {
+  margin: 0 auto;
+
+  &__circle {
+    margin: 0 auto;
+    width: 100px;
+    height: 100px;
+    background-color: rgb(38, 228, 70);
+    border-radius: 50%;
+    position: relative;
+
+    &::after {
+      content: "";
+      position: absolute;
+      top: 45%;
+      left: 50%;
+      transform: translate(-50%, -50%) rotate(45deg);
+      border-bottom: 10px solid white;
+      border-right: 10px solid white;
+      width: 15px;
+      height: 40px;
+    }
+  }
+
+  &__header {
+    text-align: center;
+    margin-top: 45px;
+  }
+}
+</style>
